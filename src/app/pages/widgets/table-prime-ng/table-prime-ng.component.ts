75a1cd4c15ff0e10a3efa6abfdba588cac4c4d12
Amr.Mossad <eng.amrmossad93@gmail.com>
2019-09-07 12:44:07 +0200

    Table

---
+++ src/app/pages/widgets/table-prime-ng/table-prime-ng.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-table-prime-ng',
+  templateUrl: './table-prime-ng.component.html',
+  styleUrls: ['./table-prime-ng.component.scss']
+})
+export class TablePrimeNgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
