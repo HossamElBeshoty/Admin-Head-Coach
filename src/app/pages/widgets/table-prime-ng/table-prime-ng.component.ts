3bf7f234dc11614e5496288fa82ecd921528585b
Amr.Mossad <eng.amrmossad93@gmail.com>
2019-09-07 12:45:26 +0200

    lint

--- src/app/pages/widgets/table-prime-ng/table-prime-ng.component.ts
+++ src/app/pages/widgets/table-prime-ng/table-prime-ng.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-table-prime-ng',
   templateUrl: './table-prime-ng.component.html',
-  styleUrls: ['./table-prime-ng.component.scss']
+  styleUrls: ['./table-prime-ng.component.scss'],
 })
 export class TablePrimeNgComponent implements OnInit {
 
