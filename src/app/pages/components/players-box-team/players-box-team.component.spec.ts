b98f35e54c38731aa1c86877a3f6ccfebf431434
AmrMossad93 <eng.amrmossad93@gmail.com>
2019-10-14 21:50:49 +0200

    Lint

--- src/app/pages/components/players-box-team/players-box-team.component.spec.ts
+++ src/app/pages/components/players-box-team/players-box-team.component.spec.ts
@@ -8,7 +8,7 @@ describe('PlayersBoxTeamComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayersBoxTeamComponent ]
+      declarations: [ PlayersBoxTeamComponent ],
     })
     .compileComponents();
   }));
