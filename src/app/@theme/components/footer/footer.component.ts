96da8a5e8958953407aa06d289275132d4db9575
AmrMossad93 <eng.amrmossad93@gmail.com>
2019-11-23 19:21:34 +0200

    Design Analysis

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by"
+      <span class="created-by"
       ><i class="fas fa-code animated swing infinite"> </i> Copyright &copy;
       2019 All Rights Reserved By
       <b>
         <a
-          href="https://wwwmasadevelopmentcom.000webhostapp.com/"
-          target="_blank"
-          >Eng.Hossam El Beshoty & Eng. Amr Mossad</a
+                href="https://wwwmasadevelopmentcom.000webhostapp.com/"
+                target="_blank"
+        >Eng.Hossam El Beshoty & Eng. Amr Mossad</a
         ></b
       ></span
-    >
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+      >
+      <div class="socials">
+          <a href="#" target="_blank" class="ion ion-social-github"></a>
+          <a href="https://www.facebook.com/amr.mossad.12979?ref=bookmarks" target="_blank"
+             class="ion ion-social-facebook"></a>
+          <a href="https://twitter.com/AmrMossad8" target="_blank" class="ion ion-social-twitter"></a>
+          <a href="https://www.linkedin.com/in/amr-mossad-209976158/" target="_blank"
+             class="ion ion-social-linkedin"></a>
+      </div>
   `,
 })
-export class FooterComponent { }
+export class FooterComponent {
+}
