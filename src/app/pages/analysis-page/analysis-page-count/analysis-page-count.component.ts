b72d42a6ad58692c1c2062068b5b6f8087178d90
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-02-15 15:27:22 +0200

    Table

--- src/app/pages/analysis-page/analysis-page-count/analysis-page-count.component.ts
+++ src/app/pages/analysis-page/analysis-page-count/analysis-page-count.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analysis-page-count.component.scss'],
 })
 export class AnalysisPageCountComponent implements OnInit {
+  countsData = [
+    {
+      actionA: 'Attack',
+      countA: 4,
+      descriptionA: 'Nice Attack',
+      actionB: 'Defence',
+      countB: 2,
+      descriptionB: 'Bad Defence',
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
