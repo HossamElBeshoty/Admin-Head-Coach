b06d466e95f9fe17b692e7d50a9ace5b65c02fc3
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-03-28 14:08:21 +0200

    Teams

--- src/app/pages/maps/maps-routing.module.ts
+++ src/app/pages/maps/maps-routing.module.ts
@@ -1,26 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { MapsComponent } from './maps.component';
-import { GmapsComponent } from './gmaps/gmaps.component';
-import { LeafletComponent } from './leaflet/leaflet.component';
-import { BubbleMapComponent } from './bubble/bubble-map.component';
-import { SearchMapComponent } from './search-map/search-map.component';
-import { MapComponent } from './search-map/map/map.component';
-import { SearchComponent } from './search-map/search/search.component';
+import {MapsComponent} from './maps.component';
+import {LeafletComponent} from './leaflet/leaflet.component';
+import {SearchMapComponent} from './search-map/search-map.component';
+import {MapComponent} from './search-map/map/map.component';
+import {SearchComponent} from './search-map/search/search.component';
 
 const routes: Routes = [{
   path: '',
   component: MapsComponent,
   children: [{
-    path: 'gmaps',
-    component: GmapsComponent,
-  }, {
     path: 'leaflet',
     component: LeafletComponent,
-  }, {
-    path: 'bubble',
-    component: BubbleMapComponent,
   }, {
     path: 'searchmap',
     component: SearchMapComponent,
@@ -31,13 +23,12 @@ const routes: Routes = [{
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class MapsRoutingModule { }
+export class MapsRoutingModule {
+}
 
 export const routedComponents = [
   MapsComponent,
-  GmapsComponent,
   LeafletComponent,
-  BubbleMapComponent,
   SearchMapComponent,
   MapComponent,
   SearchComponent,
